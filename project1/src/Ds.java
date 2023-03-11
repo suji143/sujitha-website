4a33a378950b57a1d682229b2620b6fb31c49ebc
suji143 <98933127+suji143@users.noreply.github.com>
2023-03-11 17:53:13 +0530

    Add files via upload

---
+++ project1/src/Ds.java
@@ -0,0 +1,10 @@
+import java.io.*;
+ 
+class Ds {
+    public static void main(String[] args)
+    {
+        int var = 10; // Declared a Local Variable
+        // This variable is local to this main method only
+        System.out.println("Local Variable: " + var);
+    }
+}
